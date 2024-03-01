dbb73cda5298682e445f153b9182b367b0797efc
= <bendev4977@gmail.com>
2024-03-01 04:38:26 +0100

    update

--- templates/pages/profile.php
+++ templates/pages/profile.php
@@ -3,8 +3,9 @@
 
 <section class="section-content">
 
+    <h2>Welcome <span class="bold"><?= ucfirst($_SESSION["user"]["name"]); ?></span></h2>
+
     <div class="div-content">
-        <p>Welcome <span class="bold"><?= ucfirst($_SESSION["user"]["name"]); ?></span></p>
         <p>Your email is <span class="bold"><?= $_SESSION["user"]["email"]; ?></span></p>
     </div>
 
