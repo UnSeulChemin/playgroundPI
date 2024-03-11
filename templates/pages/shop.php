91b31d66be8f58d91c191a5395fccbfb78c82dd7
= <bendev4977@gmail.com>
2024-03-12 00:13:07 +0100

    update

--- templates/pages/shop.php
+++ templates/pages/shop.php
@@ -1,4 +1,4 @@
-<?php $title = "Shop"; ?>
+<?php $title = (isset($_GET["id"])) ? "Shop " . $_GET["id"] : "Shop"; ?>
 <?php ob_start(); ?>
 
 <section class="section-content">
@@ -9,35 +9,36 @@
         <?php foreach($shops as $shop): ?>
             <div class="div-card">
                 <figure class="figure-card">
-                    <img src="<?= isset($_GET['id']) ? '../' : null; ?>public/images/shop/<?= $shop['name'].".".$shop['extension']; ?>">
+                    <img src="<?= isset($_GET["id"]) ? "../" : null; ?>public/images/shop/<?= $shop["name"].".".$shop["extension"]; ?>">
                 </figure>
                 <div class="div-card-content">
-                    <p class="margin-none"><?= $shop['description']; ?></p>
+                    <p class="margin-none"><?= $shop["description"]; ?></p>
                 </div>
                 <div class="div-card-content">
-                    <p class="margin-none"><?= date('d/m/Y', strtotime($shop['created_at'])); ?></p>
+                    <p class="margin-none"><?= date("d/m/Y", strtotime($shop["created_at"])); ?></p>
                 </div>
             </div>
         <?php endforeach; ?>
     </section>
 
     <nav class="flex-center margin-top">
-        <?php for ($count = 1; $count <= $countPage; $count++): ?>
+        <?php for ($count = 1; $count <= $countPage; $count++):
         
-            <?php if (!isset($getId)):
+            if (!isset($getId)):
                 $getId = 1;
-            endif; ?>
+            endif;
 
-            <?php if ($getId != $count):
-                if (isset($_GET['id']) && !empty($_GET['id'])): ?>
+            if ($getId != $count):
+                if (isset($_GET["id"]) && !empty($_GET["id"])): ?>
                     <a class="link-paginate" href="../shop/<?php echo $count; ?>"><?php echo $count; ?></a>               
                 <?php else: ?>
                     <a class="link-paginate" href="shop/<?php echo $count; ?>"><?php echo $count; ?></a>
                 <?php endif; ?>
             <?php else: ?>
                 <a class="link-paginate active"><?php echo $count; ?></a>
-            <?php endif; ?>
-        <?php endfor; ?>
+            <?php endif;
+
+        endfor; ?>
     </nav>
 
 </section>
